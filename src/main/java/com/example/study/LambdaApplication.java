32a658ece12b02def793902e7874abea453a42d7
loveleen <loveleen.narang@gmail.com>
2020-01-09 14:29:35 -0500

    formatted

--- src/main/java/com/example/study/LambdaApplication.java
+++ src/main/java/com/example/study/LambdaApplication.java
@@ -5,13 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LambdaApplication implements CommandLineRunner{
+public class LambdaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LambdaApplication.class, args);
 	}
 
-	 public void run(String... arg0) throws Exception {
-	      System.out.println("Hello world from Command Line Runner");
-	   }
+	public void run(String... arg0) throws Exception {
+		System.out.println("Hello world from Command Line Runner");
+	}
 }
