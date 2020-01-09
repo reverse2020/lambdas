541f56a9a56826cd02a1592aae8e1a626c0acd54
loveleen <loveleen.narang@gmail.com>
2020-01-09 14:35:10 -0500

    IntStream min max example added

--- src/main/java/com/example/study/LambdaApplication.java
+++ src/main/java/com/example/study/LambdaApplication.java
@@ -1,5 +1,7 @@
 package com.example.study;
 
+import java.util.stream.IntStream;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,5 +15,10 @@ public class LambdaApplication implements CommandLineRunner {
 
 	public void run(String... arg0) throws Exception {
 		System.out.println("Hello world from Command Line Runner");
+        IntStream stream = IntStream.of(-9, -18, 54, 8, 7, 14, 3,-99); 
+
+        System.out.println(stream.min().getAsInt());
+        System.out.println(IntStream.of(-9, -18, 54, 8, 7, 14, 3,-99).max().getAsInt());
+
 	}
 }
