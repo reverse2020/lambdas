549dd9aede832db2a9021f4261573447cc5d3b3c
loveleen <loveleen.narang@gmail.com>
2020-01-09 14:40:38 -0500

    theory added

--- src/main/java/com/example/study/LambdaApplication.java
+++ src/main/java/com/example/study/LambdaApplication.java
@@ -14,10 +14,22 @@ public class LambdaApplication implements CommandLineRunner {
 	}
 
 	public void run(String... arg0) throws Exception {
+		
+		
+		
 		System.out.println("Hello world from Command Line Runner");
         IntStream stream = IntStream.of(-9, -18, 54, 8, 7, 14, 3,-99); 
 
         System.out.println(stream.min().getAsInt());
+        
+        // A stream can be used only once.
+		/*
+		 * “A stream should be operated on (invoking an intermediate or terminal stream
+		 * operation) only once. This rules out, for example, “forked” streams, where
+		 * the same source feeds two or more pipelines, or multiple traversals of the
+		 * same stream. A stream implementation may throw IllegalStateException if it
+		 * detects that the stream is being reused.”
+		 */        
         System.out.println(IntStream.of(-9, -18, 54, 8, 7, 14, 3,-99).max().getAsInt());
 
 	}
