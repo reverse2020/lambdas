a41feca54b4b962654f218676020fc73a92d4d15
Narang <lnarang@deloitte.com>
2020-01-09 14:12:32 -0500

    added command line runner

--- src/main/java/com/example/study/LambdaApplication.java
+++ src/main/java/com/example/study/LambdaApplication.java
@@ -1,13 +1,17 @@
 package com.example.study;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LambdaApplication {
+public class LambdaApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LambdaApplication.class, args);
 	}
 
+	 public void run(String... arg0) throws Exception {
+	      System.out.println("Hello world from Command Line Runner");
+	   }
 }
